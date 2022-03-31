d2501fcf0a9eab602967589a1fcc21e50c56e021
ericxu12 <99890720+ericxu12@users.noreply.github.com>
2022-03-31 14:25:31 -0400

    Add files via upload

--- app/src/main/java/com/example/theguessinggame/MainActivity.java
+++ app/src/main/java/com/example/theguessinggame/MainActivity.java
@@ -1,30 +1,35 @@
-package com.example.theguessinggame;
+package com.example.playagainoption;
 
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.PopupWindow;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button play;
+    private Button done;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Button button2 = (Button) findViewById(R.id.button2);
-
-        button2.setOnClickListener(new View.OnClickListener() {
-
+        done=findViewById(R.id.Done);
+        done.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, Pop.class));
+                Intent intent=new Intent(MainActivity.this,EndGameScreen.class);
+                startActivity(intent);
+            }
+        });
+        play=findViewById(R.id.Play);
+        play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(MainActivity.this,MainScreen.class);
+                startActivity(intent);
+
             }
         });
 
