3051a9e749d68c1de9b10872a62754c3084d2e15
ericxu12 <99890720+ericxu12@users.noreply.github.com>
2022-04-01 17:27:40 -0400

    Add files via upload

---
+++ app/src/main/java/com/example/theguessinggame/MainActivity2.java
@@ -0,0 +1,37 @@
+package com.example.playagainoption;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity2 extends AppCompatActivity {
+    private Button play;
+    private Button done;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        done=findViewById(R.id.Done);
+        done.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(MainActivity2.this,EndGameScreen.class);
+                startActivity(intent);
+            }
+        });
+        play=findViewById(R.id.Play);
+        play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(MainActivity2.this,MainScreen.class);
+                startActivity(intent);
+
+            }
+        });
+
+    }
+}
