9411ed7d883fdf9b7e3eb3e7cbc92c448ae9307d
ericxu12 <99890720+ericxu12@users.noreply.github.com>
2022-04-02 19:20:48 -0400

    Update hint.java

--- app/src/main/java/com/example/theguessinggame/hint.java
+++ app/src/main/java/com/example/theguessinggame/hint.java
@@ -32,9 +32,7 @@ public class hint extends AppCompatActivity {
         exit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                moveTaskToBack(true);
-                android.os.Process.killProcess(android.os.Process.myPid());
-                System.exit(1); 
+                Intent intent = new Intent( 
             }
         });
         
