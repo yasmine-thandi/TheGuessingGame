73ed8c46c479d580416a2ea65b405baee9e916c9
Yasmine <91900711+yasmine-thandi@users.noreply.github.com>
2022-04-01 22:10:01 -0400

    Update MainScreen.java

--- app/src/main/java/com/example/theguessinggame/MainScreen.java
+++ app/src/main/java/com/example/theguessinggame/MainScreen.java
@@ -1,4 +1,4 @@
-package com.example.playagainoption;
+package com.example.theguessinggame;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,6 +9,6 @@ public class MainScreen extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_screen);
+        setContentView(R.layout.activity_main);
     }
 }
