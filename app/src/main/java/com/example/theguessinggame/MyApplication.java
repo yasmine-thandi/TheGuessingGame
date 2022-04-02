1ec6259a2f30b30f38344308e0f5f2d32f5d5f4e
Brandon Petherick <99896226+BranEst@users.noreply.github.com>
2022-04-02 17:15:39 -0400

    Update MyApplication.java

--- app/src/main/java/com/example/theguessinggame/MyApplication.java
+++ app/src/main/java/com/example/theguessinggame/MyApplication.java
@@ -5,6 +5,19 @@ import android.app.Application;
 public class MyApplication extends Application {
 
     private int highScore = 0;
+    private int target = 0;
+    private int attempts = 10;
+
+
+    public int getTarget() {return target;}
+
+    public void setTarget(int score) {target = score;}
+
+    public void resetAttempts() {attempts = 10;}
+
+    public void attempted() {attempts--;}
+
+    public int getAttempts() {return attempts;}
 
     public int getHighScore() {
         return highScore;
